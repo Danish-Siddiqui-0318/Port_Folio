6b818f0fd731b69abd76c67b9dc3d2374574b6b5
Danish-Siddiqui-0318 <contact.dasiddique@gmail.com>
2024-08-03 19:27:33 +0500

    completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,10 @@ const Navbar = () => {
         <h1 className="text-lg mx-2 uppercase">Portfolio</h1>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
-        <a href="www.linkedin.com/in/danish-siddiqui-6a6648253" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/danish-siddiqui-6a6648253/"
+          target="_blank"
+        >
           <FaLinkedin />
         </a>
         <a href="https://github.com/Danish-Siddiqui-0318" target="_blank">
